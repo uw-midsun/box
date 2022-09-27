94d4af75fdbc42263a02e679acd76f3c2885bbc4
ShiCheng Lu <shichenglu0708@gmail.com>
2022-09-27 19:41:08 -0400

    update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,4 @@
-echo "HELLO"
+
+whoami
+
+echo "$EUID"
--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
-echo "SETTING UP"
+echo "checking requirements"
+
+wget -q https://raw.githubusercontent.com/uw-midsun/box/requirements/requirements.sh -O requirements.sh
+
+if cmp --silent -- "requirements.sh" "completed_requirements.sh"; then
+  echo "setup up to date"
+  exit
+fi
+
+./requirements.sh
+
+mv "requirements.sh" "completed_requirements.sh"
+ls
