4e3b5ed65d36e5a0345fdac4721e0b5e2b4f406c
ShiCheng Lu <shichenglu0708@gmail.com>
2022-09-27 20:48:18 -0400

    added all requirements

--- setup.sh
+++ setup.sh
@@ -1,13 +1,20 @@
-echo "checking requirements"
+echo "Checking requirements"
 
-wget -q https://raw.githubusercontent.com/uw-midsun/box/requirements/requirements.sh -O requirements.sh
+# first time this script is ran, vm might not have anything
+[[ -f "completed_requirements.sh" ]] || apt update; apt install apt-get curl
+
+# rm "requirements.sh" "completed_requirements.sh"
+
+curl -s https://raw.githubusercontent.com/uw-midsun/box/requirements/requirements.sh -O "requirements.sh"
 
 if cmp --silent -- "requirements.sh" "completed_requirements.sh"; then
-  echo "setup up to date"
+  echo "Requirements up to date"
   exit
 fi
 
+echo "Installing new requirements..."
+
+chmod +x requirements.sh
 ./requirements.sh
 
 mv "requirements.sh" "completed_requirements.sh"
-ls
